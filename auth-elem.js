51ad8b54a50935f884f0f61fc49002d184478b0b
UP2002348 <91694918+UP2002348@users.noreply.github.com>
2023-08-31 19:51:13 +0100

    Improved moduler structure and refactored css code

--- auth-elem.js
+++ auth-elem.js
@@ -6,12 +6,11 @@ class AuthElem extends HTMLElement{
         let username = "Mehmet Mazi";
         const shadow = this.attachShadow({mode:"closed"});
         const link = document.createElement('link');
-        link.setAttribute('href', "style.css");
+        link.setAttribute('href', "custom-elem-style.css");
         link.setAttribute('rel', 'stylesheet');
         link.setAttribute('type', 'text/css');
         shadow.append(link);
         if (!loggedIn){
-            console.log("ping");
             const unauthenticated = document.querySelector("#unauthenticated");
             const cloned = unauthenticated.content.cloneNode(true);
             shadow.append(cloned);
